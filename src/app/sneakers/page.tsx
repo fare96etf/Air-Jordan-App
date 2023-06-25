9e8f0413619d0a52a7c95b79f42e8c5c3d480cfa
Faris Kantic <faris_kantic@hotmail.com>
2023-06-25 10:57:05 +0200

    search sneakers by description

--- src/app/sneakers/page.tsx
+++ src/app/sneakers/page.tsx
@@ -1,13 +1,34 @@
+"use client"
+
 import { sneaker_list } from "@/data/sneaker_list"
 import Link from "next/link"
+import { useState } from "react";
 
 export default function SneakerList() {
+    const [sneakers, setSneakers] = useState(sneaker_list);
+
+    const inputHandler = (e: any) => {
+        if (e.target?.value?.trim() === "") {
+            setSneakers(sneaker_list);
+            return;
+        }
+
+        const lowerCase = e.target?.value?.toLowerCase();
+        setSneakers(sneaker_list.filter(sn => sn.shortDescription.toLowerCase().includes(lowerCase)));
+    };
+
     return (
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <h1 className="text-3xl font-bold mt-8 mb-4">Sneakers</h1>
+            <input
+                className="border-4 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                type="text"
+                placeholder="Search by description..."
+                onChange={inputHandler} 
+            />
             <div className="prose">
                 <div className="grid gap-6 lg:grid-cols-3">
-                    {sneaker_list.map((item) => (
+                    {sneakers.map((item) => (
                         <div key={item.id} className="bg-white rounded-lg shadow-md p-6">
                             <Link href={`/sneakers/${item.id}`}>
                                 <img className="h-40 w-full object-cover mb-4 rounded-lg" src={item.imageUrl} alt={item.title} />
